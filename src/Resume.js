02152bbeceff33d1d0bcde21a7e9bf39b5cca89a
henry <henry.macafee@gmail.com>
2020-10-19 16:33:07 -0500

    Resume updated

--- src/Resume.js
+++ src/Resume.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react'
 import ReactSVG from 'react-svg'
-import henryResume from './assets/henryresume.svg';
+import henryResume1 from './assets/henryresume1.svg'
 import henryResumePdf from './assets/henryresume.pdf'
 import downloadIcon from './assets/download.svg'
 import classes from './Resume.module.css';
 
 
-class Resume extends Component  {
-    render() {
+class Resume extends Component {
+  render() {
 
-        return(
-            <div className={classes.container}>
-                <a href={henryResumePdf}><ReactSVG src={henryResume}
-                  beforeInjection={svg => {
-                svg.classList.add(classes['henry-resume'])
-    svg.setAttribute('style', 'font-family: Oswald')
-  }}/><div className={classes.overlay}><img alt="download" className={classes['download-icon']} src={downloadIcon}></img></div></a>
-
+    return (
+      <div className={classes.container}>
+        <a href={henryResumePdf}><ReactSVG src={henryResume1}
+          beforeInjection={svg => {
+            svg.classList.add(classes['henry-resume'])
+            svg.setAttribute('style', 'font-family: Oswald')
+          }} /><div className={classes.overlay}><img alt="download" className={classes['download-icon']} src={downloadIcon}></img></div></a>
       </div>
-        )
-    }
-     }
+    )
+  }
+}
 
 export default Resume;
